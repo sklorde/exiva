12d5e19c12e6427c7426b14a187c9647b2436a59
sklorde <bruno.assis132@gmail.com>
2023-03-20 08:55:55 -0300

    :recycle: cleaning the code

--- lib/app/lib/app/widgets/character_card.dart
+++ lib/app/lib/app/widgets/character_card.dart
@@ -1,6 +1,6 @@
+import 'package:exiva/app/lib/app/widgets/character_detail.dart';
 import 'package:exiva/app/shared/models/player.dart';
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 class CharacterCard extends StatelessWidget {
   final Player player;
@@ -32,70 +32,7 @@ class CharacterCard extends StatelessWidget {
           ),
           body: TabBarView(
             children: [
-              Column(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Column(
-                    children: [
-                      Transform.scale(
-                        scale: 0.8,
-                        child: FutureBuilder(
-                          builder: (BuildContext context,
-                              AsyncSnapshot<void> snapshot) {
-                            if (snapshot.connectionState ==
-                                ConnectionState.waiting) {
-                              return const Center(
-                                child: CircularProgressIndicator(),
-                              );
-                            } else {
-                              return Image.asset(
-                                vocationImages[
-                                    player.characters?.character!.vocation]!,
-                              );
-                            }
-                          },
-                        ),
-                      ),
-                      Text(
-                        '${(player.characters?.character!.vocation)?.toUpperCase()}',
-                        style: GoogleFonts.poppins(
-                          textStyle: const TextStyle(
-                            fontSize: 14,
-                            color: Colors.grey,
-                            letterSpacing: 1.3,
-                            fontWeight: FontWeight.w500,
-                          ),
-                        ),
-                      ),
-                      Text(
-                        '${player.characters?.character!.name}',
-                        style: GoogleFonts.poppins(
-                          textStyle: const TextStyle(
-                            fontSize: 19,
-                            fontWeight: FontWeight.w600,
-                          ),
-                        ),
-                        textAlign: TextAlign.center,
-                      ),
-                    ],
-                  ),
-                  Expanded(
-                    child: GridView.count(
-                      crossAxisCount: 3,
-                      mainAxisSpacing: 16.0,
-                      crossAxisSpacing: 16.0,
-                      childAspectRatio: 1.0,
-                      physics: const NeverScrollableScrollPhysics(),
-                      children: <Widget>[
-                        _buildInfo('LEVEL'),
-                        _buildInfo('SEX'),
-                        _buildInfo('WORLD'),
-                      ],
-                    ),
-                  )
-                ],
-              ),
+              CharacterDetail(player: player),
               const Icon(Icons.warning, size: 350),
               const Icon(Icons.rocket_launch_rounded, size: 350),
             ],
@@ -104,68 +41,4 @@ class CharacterCard extends StatelessWidget {
       ),
     );
   }
-
-  SizedBox _buildInfo(String title) {
-    String? valor;
-    switch (title) {
-      case 'LEVEL':
-        valor = player.characters?.character?.level?.toString();
-        break;
-      case 'SEX':
-        valor = player.characters?.character?.sex;
-        break;
-      case 'WORLD':
-        valor = player.characters?.character?.world;
-        break;
-      default:
-        valor = '';
-    }
-    return SizedBox(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Text(
-            title,
-            style: GoogleFonts.poppins(
-              textStyle: const TextStyle(
-                fontSize: 14,
-                color: Colors.grey,
-                letterSpacing: 1.3,
-                fontWeight: FontWeight.w500,
-              ),
-            ),
-          ),
-          Text(
-            valor.toString().toUpperCase(),
-            style: GoogleFonts.poppins(
-              textStyle: const TextStyle(
-                fontSize: 16,
-                fontWeight: FontWeight.w500,
-              ),
-            ),
-            textAlign: TextAlign.center,
-          ),
-        ],
-      ),
-    );
-  }
 }
-
-final Map<String, String> vocationImages = {
-  'Druid': 'lib/app/shared/utils/images/druid.png',
-  'Elder Druid': 'lib/app/shared/utils/images/druid.png',
-  'Knight': 'lib/app/shared/utils/images/knight.png',
-  'Elite Knight': 'lib/app/shared/utils/images/knight.png',
-  'Sorcerer': 'lib/app/shared/utils/images/sorcerer.png',
-  'Master Sorcerer': 'lib/app/shared/utils/images/sorcerer.png',
-  'Paladin': 'lib/app/shared/utils/images/paladin.png',
-  'Royal Paladin': 'lib/app/shared/utils/images/paladin.png',
-};
-
-
-// Transform.scale(
-//                     scale: 0.35,
-//                     child:
-//                         Image.asset('lib/app/shared/utils/images/male.png'),
-//                   ),
