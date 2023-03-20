90698e669846efb76849094c82f789b18fd4ee54
Bruno <53619830+sklorde@users.noreply.github.com>
2023-03-19 22:03:31 -0300

    :recycle: remove print

--- lib/app/shared/repositories/tibia_datasource.dart
+++ lib/app/shared/repositories/tibia_datasource.dart
@@ -12,10 +12,6 @@ class TibiaDataSource {
     try {
       final result = await dio.get('/character/$name');
 
-      Player player = Player.fromJson(result.data);
-
-      print(player.characters?.character!.vocation);
-
       return Player.fromJson(result.data);
     } catch (error) {
       throw Exception();
